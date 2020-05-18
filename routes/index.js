65b8729f3b8d1e4cef7c7e52350dcbaf73596ee6
unknown <ricardo.costa_loureiro@hotmail.com>
2020-05-18 14:15:56 +0100

    Added next/previous search page button and functionality, added food image display

--- routes/index.js
+++ routes/index.js
@@ -11,17 +11,49 @@ router.get('/', (req, res, next) => {
 
 router.get('/search', (req, res, next) => {
   const searchTerm = req.query.name;
+  const direction = req.query.direction;
+  const nextPage = Math.max(0, Number(req.query.session) + Number(direction));
+  console.log(req.query.session);
   axios
     .get(
-      `https://api.edamam.com/api/food-database/parser?ingr=${searchTerm}&app_id=${process.env.EDAMAM_APP_ID}&app_key=${process.env.EDAMAM_APP_KEY}`
+      `https://api.edamam.com/api/food-database/parser?session=${nextPage *
+        44}&ingr=${searchTerm}&app_id=${process.env.EDAMAM_APP_ID}&app_key=${
+        process.env.EDAMAM_APP_KEY
+      }`
     )
 
     .then((results) => {
+      console.log(results.data.hints[0]);
       const foodData = results.data.hints;
-      res.render('search-results', { foodData });
+      const searchTerm = results.data.text;
+      res.render('search-results', { foodData, searchTerm, nextPage });
     })
     .catch((error) => {
       next(error);
+    });
+});
+
+router.get('/food/:id', (req, res, next) => {
+  const foodId = req.params.id;
+  const data = {
+    ingredients: [
+      {
+        quantity: 1,
+        measureURI: 'http://www.edamam.com/ontologies/edamam.owl#Measure_unit',
+        foodId: foodId,
+      },
+    ],
+  };
+  axios
+    .post(
+      `https://api.edamam.com/api/food-database/nutrients?app_id=${process.env.EDAMAM_APP_ID}&app_key=${process.env.EDAMAM_APP_KEY}`,
+      data
+    )
+    .then((result) => {
+      const foodDetails = result.data;
+      const foodInfo = result.data.ingredients[0].parsed[0];
+      console.log(result.data);
+      res.render('single-view', { foodDetails, foodInfo });
     });
 });
 
